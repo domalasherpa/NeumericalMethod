883cac83d3d2f609c1ce6578be0936d6c6e78805
domalasherpa <domalasherpa9813@gmail.com>
2022-09-07 20:38:37 +0545

    newton rapson method

--- Unit_I/newton_rapson.cc
+++ Unit_I/newton_rapson.cc
@@ -8,39 +8,43 @@
         where approx > x
 **/
 
-
 int a, b, c, d;
+static int i;
 
-float f(float x){
-   return a * (x*x*x) + b* (x*x) + c*x+ d;
-   //return  sinf(x) - 5 * x + 6;
-
+float f(float x)
+{
+    return a * (x * x * x) + b * (x * x) + c * x + d;
+    // return  sinf(x) - 5 * x + 6;
 }
 
-float fd(float x){
+float fd(float x)
+{
     float approx = x + 0.001;
     float rslt = (f(approx) - f(x)) / (approx - x);
     return rslt;
 }
 
-void newton_rapson(float *x){
+void newton_rapson(float *x)
+{
 
-    float new_x = *x - f(*x)/fd(*x);
-   
-    float error = abs((new_x - *x) /new_x);
+    float new_x = *x - f(*x) / fd(*x);
 
-    //setting old value with new value
+    float error = abs((new_x - *x) / new_x);
+
+    // setting old value with new value
     *x = new_x;
-    std::cout << *x<< "\n";
 
-    if(error >= 0.005f){ 
-        //feeding the new value again to func
+    if (error >= 0.005f)
+    {
+        // feeding the new value again to func
+        i++;
         newton_rapson(x);
-    } 
+    }
     return;
 }
 
-int main(){
+int main()
+{
 
     float guess;
     std::cout << "Enter a, b, c, and d: ";
@@ -48,6 +52,7 @@ int main(){
     std::cout << "Enter a inital guess: ";
     std::cin >> guess;
     newton_rapson(&guess);
-    std::cout <<  "root is: "<<guess;
-
+    std::cout << "root is: " << guess << "\n";
+    std::cout << "Functional value: " << f(guess) << "\n";
+    std::cout << "Iterations: " << i;
 }
