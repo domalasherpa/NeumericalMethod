05d2949f97c014b00d08f2f4cb78124ad6f96f1d
domalasherpa <domalasherpa9813@gmail.com>
2022-09-07 20:57:05 +0545

    fixed point method

--- Unit_I/fixed_point.cc
+++ Unit_I/fixed_point.cc
@@ -4,20 +4,32 @@
 
 using namespace std;
 
-float g(float *x){
-    return pow((*x + 10), (1.0/4.0));  
+static int iter;
+
+float f(float x)
+{
+    return (pow(x, 4) - x - 10);
 }
 
-void fixed_point(float *x0, float *x1){
+float g(float *x)
+{
+    return pow((*x + 10), (1.0 / 4.0));
+}
 
-    static int iter = 1;
-    
+void fixed_point(float *x0, float *x1)
+{
+
     *x1 = g(x0);
-    float error = abs((*x1 - *x0)/ *x1);
+    float error = ((*x1 - *x0) / *x1);
+    if (error < 0)
+    {
+        error *= -1;
+    }
     //__display
-    cout << setw(10) << iter << setw(10) << *x0 << setw(10) << *x1 << setw(15) << error << "\n";
+    // cout << setw(10) << iter << setw(10) << *x0 << setw(10) << *x1 << setw(15) << error << "\n";
 
-    if(error >= 0.005f){
+    if (error >= 0.005f)
+    {
         *x0 = *x1;
         iter++;
         fixed_point(x0, x1);
@@ -25,17 +37,19 @@ void fixed_point(float *x0, float *x1){
     return;
 }
 
-int main(){
+int main()
+{
 
     float x0, x1;
 
     cout << "Enter initial guess: ";
     cin >> x0;
 
-    cout << setw(10) << "iteration" << setw(10) << "Xn" << setw(10) << "Xn+1" << setw(10) << "error" << "\n";
+    // cout << setw(10) << "iteration" << setw(10) << "Xn" << setw(10) << "Xn+1" << setw(10) << "error" << "\n";
 
     fixed_point(&x0, &x1);
-    
-    cout << "Root is: " << x1;
-    
+
+    cout << "Root is: " << x1 << "\n";
+    cout << "Functional value is: " << f(x1) << "\n";
+    cout << "Iterations: " << iter;
 }
