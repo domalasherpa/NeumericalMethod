938f9ff61712ab5737e04ff9b91b268f0a1ff929
domalasherpa <domalasherpa9813@gmail.com>
2022-09-07 20:34:10 +0545

    secant method

--- Unit_I/secant.cc
+++ Unit_I/secant.cc
@@ -4,20 +4,25 @@
 
 using namespace std;
 
-float f(float *x){
+static int i;
+float f(float *x)
+{
     return sinf(*x) - 5 * (*x) + 6;
 }
 
-void secant(float *x0, float *x1, float *x2){
-    
-    *x2 = *x1 - (*x1 - *x0) * f(x1)/(f(x1) - f(x0));
-    float error= abs((*x2 - *x1) / *x2);
+void secant(float *x0, float *x1, float *x2)
+{
+
+    *x2 = *x1 - (*x1 - *x0) * f(x1) / (f(x1) - f(x0));
+    float error = abs((*x2 - *x1) / *x2);
 
     //__Display
-    cout << setw(15) << *x0 << setw(15) <<f(x0) << setw(15) << *x1 
-        << setw(15) << f(x1) << setw(15) << *x2 << setw(15) << f(x2) << setw(15) <<error << "\n";
-        
-    if(error >= 0.005f){
+    // cout << setw(15) << *x0 << setw(15) <<f(x0) << setw(15) << *x1
+    //     << setw(15) << f(x1) << setw(15) << *x2 << setw(15) << f(x2) << setw(15) <<error << "\n";
+
+    if (error >= 0.005f)
+    {
+        i++;
         *x0 = *x1;
         *x1 = *x2;
         return secant(x0, x1, x2);
@@ -25,18 +30,22 @@ void secant(float *x0, float *x1, float *x2){
     return;
 }
 
-int main(){
+int main()
+{
 
     float x0, x1, x2;
 
     cout << "Enter two inital guess: ";
     cin >> x0 >> x1;
 
-    cout << setw(15) << "x0" << setw(15) <<"f(x0)" << setw(15) << "x1" 
-        << setw(15) << "f(x1)" << setw(15) << "x2"<< setw(15) << "f(x2)" << setw(15)<< "error" << "\n";
+    // cout << setw(15) << "x0" << setw(15) << "f(x0)" << setw(15) << "x1"
+    //      << setw(15) << "f(x1)" << setw(15) << "x2" << setw(15) << "f(x2)" << setw(15) << "error"
+    //      << "\n";
 
     secant(&x0, &x1, &x2);
 
     cout << "The root is : " << x2 << "\n";
+    cout << "Functional value: " << f(&x2) << "\n";
+    cout << "Iterations: " << i;
     return 0;
 }
