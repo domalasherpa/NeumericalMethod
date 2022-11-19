037feac70848ba5fbac026f552b9cc3e84e5dbfe
domalasherpa <domalasherpa9813@gmail.com>
2022-11-19 19:42:58 +0545

    jacobi iteration

--- Unit_IV/jacobi.cpp
+++ Unit_IV/jacobi.cpp
@@ -1,6 +1,7 @@
 //jacobi method for linear qequation with three variables.
 
 #include <iostream>
+#include <cmath>
 #define ROW 3
 #define COL 3
 
@@ -31,14 +32,55 @@ void jacobi(float *x, int a[][COL] , int b[], int iter){
 
 }
 
+void swap(int (*a)[COL], int *b, int row, int xrow){
+    int temp;
+    for(int i = 0; i < COL; i++){
+        temp = a[row][i];
+        a[row][i] = a[xrow][i];
+        a[xrow][i] = temp;
+    }
+    temp = b[row];
+    b[row] = b[xrow];
+    b[xrow] = temp;
+}
+
+void makeDiagonal(int (*a)[COL], int *b){
+    for(int i = 0; i < COL; i++){
+        for(int j = i + 1; j < COL; j++){
+            if(a[i][i] < a[j][i]){
+                swap(&a[0], b, i, j);
+            }
+        }
+    }
+}
+
 int main(){
     //should make a diagonally dominent
-    int a[][COL] = {{6, -2, 1}, {-2, 7, 2}, {1, 2, -5}};
-    float x[ROW] = {0};
-    int  b[] = {11, 5, -1};
+    int a[COL][COL], b[COL], iter;
+    float x[COL] = {0};
 
+
+    //take the inputs from the user
+    cout << "Enter the coef matrix: \n";
+    for(int i = 0; i < COL; i++){
+        for(int j = 0; j < COL; j++){
+            cin >> a[i][j] ;
+        }
+    }
+
+    //take the constants
+    cout << "\nEnter the constants: \n";
+    for(int i = 0; i < COL; i++){
+        cin >> b[i];
+    }
+
+    cout << "\nEnter the number of iterations: ";
+    cin >> iter;
+
+
+    makeDiagonal(&a[0], b);
     jacobi(x, a, b, 4 );
 
     for(int i = 0; i < ROW; i++)
-    cout << x[i] <<"\t";
+    cout << round(x[i]) <<"\t";
 }
