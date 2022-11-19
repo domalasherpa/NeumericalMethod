93ba388b9a4f3a6911b1da6eb10e41e79886d049
domalasherpa <domalasherpa9813@gmail.com>
2022-11-19 19:39:18 +0545

    gauss siedel

--- Unit_IV/gauss_siedel.cpp
+++ Unit_IV/gauss_siedel.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-#define ROW 3
+
 #define COL 3
 
 using namespace std;
@@ -8,7 +8,7 @@ void gauss(float *x, int a[][COL], int b[], int iter){
 
     int i = 0;
     while(i < iter){
-        for(int i = 0; i < ROW; i++){
+        for(int i = 0; i < COL; i++){
             float exp = 0;
             for(int j = 0; j < COL; j++){
 
@@ -21,15 +21,56 @@ void gauss(float *x, int a[][COL], int b[], int iter){
         i++;
     }
 }
+
+void swap(int (*a)[COL], int *b, int row, int xrow){
+    int temp;
+    for(int i = 0; i < COL; i++){
+        temp = a[row][i];
+        a[row][i] = a[xrow][i];
+        a[xrow][i] = temp;
+    }
+    temp = b[row];
+    b[row] = b[xrow];
+    b[xrow] = temp;
+}
+
+void makeDiagonal(int (*a)[COL], int *b){
+    for(int i = 0; i < COL; i++){
+        for(int j = i + 1; j < COL; j++){
+            if(a[i][i] < a[j][i]){
+                swap(&a[0], b, i, j);
+            }
+        }
+    }
+}
+
 int main(){
-    //should make the coeff matrix diagonally dominent
-    int a[][COL] = {{6, -2, 1}, {-2, 7, 2}, {1, 2, -5}};
-    float x[ROW] = {0};
-    int  b[] = {11, 5, -1};
+    int a[COL][COL], b[COL], iter;
+    float x[COL] = {0};
 
-    gauss(x, a, b, 3 );
 
-    for(int i = 0; i < ROW; i++)
+    //take the inputs from the user
+    cout << "Enter the coef matrix: \n";
+    for(int i = 0; i < COL; i++){
+        for(int j = 0; j < COL; j++){
+            cin >> a[i][j] ;
+        }
+    }
+
+    //take the constants
+    cout << "\nEnter the constants: \n";
+    for(int i = 0; i < COL; i++){
+        cin >> b[i];
+    }
+
+    cout << "\nEnter the number of iterations: ";
+    cin >> iter;
+
+
+    makeDiagonal(&a[0], b);
+    gauss(x, a, b, iter);
+
+    for(int i = 0; i < COL; i++)
     cout << x[i] <<"\t";
 
     return 0;
